a6cd65ef7b77ddb6047c5dd5d83e820170484a3a
Keu.ni <skcor8909@gmail.com>
2023-05-05 16:49:33 +0900

    <feat> 사용자 정보 유효성 검사 후 에러 없을 시 추가

---
+++ src/middleware/deserializeUser.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from 'express';
+import { findUserById } from '../services/user.service';
+import AppError from '../utils/appError';
+import redisClient from '../utils/connectRedis';
+import { verifyJwt } from '../utils/jwt';
+
+export const deserializeUser = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    let access_token;
+
+    if (request.headers.authorization && request.headers.authorization.startsWith('Bearer')) {
+      access_token = request.headers.authorization.split(' ')[1];
+    } else if (request.cookies.accessToken) {
+      access_token = request.cookies.accessToken;
+    }
+
+    if (!access_token) {
+      return next(new AppError('You are not logged in', 401));
+    }
+    
+    const decoded = verifyJwt<{ sub: string }>(access_token);
+
+    if (!decoded) {
+      return next(new AppError(`Invalid token or user doesn't exist`, 401));
+    }
+
+    const session = await redisClient.get(decoded.sub);
+
+    if (!session) {
+      return next(new AppError(`User session has expired`, 401));
+    }
+
+    const user = await findUserById(JSON.parse(session).id);
+
+    if (!user) {
+      return next(new AppError(`User with that token no longer exist`, 401));
+    }
+
+    response.locals.user = user;
+
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+};
