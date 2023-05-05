e07663ecb8dde6144738ee1895cc95803e55e2fc
Keu.ni <skcor8909@gmail.com>
2023-05-05 13:46:09 +0900

    <feat> 사용자 검증 middleware 구현

---
+++ src/middleware/validate.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from 'express';
+import { AnyZodObject, ZodError } from 'zod';
+
+export const validate = (schema: AnyZodObject) => {
+  (request: Request, response: Response, next: NextFunction) => {
+    try {
+      schema.parse({
+        params: request.params,
+        query: request.query,
+        body: request.body,
+      });
+
+      next();
+    } catch (error: any) {
+      if (error instanceof ZodError) {
+        return response.status(400).json({
+          status: 'fail',
+          error: error.errors,
+        });
+      }
+
+      next(error);
+    }
+  };
+};
