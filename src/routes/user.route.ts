68fcf19ca1e73c35beca05561ab8668059c39cd8
Keu.ni <skcor8909@gmail.com>
2023-05-05 16:53:57 +0900

    <feat> 인증 경로 생성

---
+++ src/routes/user.route.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { getAllUsersHandler, getHandler } from '../controllers/user.controller';
+import { deserializeUser } from '../middleware/deserializeUser';
+import { requireUser } from '../middleware/requireUser';
+import { restrictTo } from './../middleware/restrictTo';
+
+const router = express.Router();
+router.use(deserializeUser, requireUser);
+
+router.get('/', restrictTo('admin'), getAllUsersHandler);
+
+router.get('/me', getHandler);
+
+export default router;
