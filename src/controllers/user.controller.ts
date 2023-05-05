3387c7d50389ce8140ab336912a97deca819a275
Keu.ni <skcor8909@gmail.com>
2023-05-05 14:21:55 +0900

    <feat> 유저 컨트롤러 생성

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+import { findAllUsers } from '../services/user.service';
+export const getHandler = (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const user = response.locals.user;
+
+    response.status(200).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  } catch (error: any) {
+    next(error);
+  }
+};
+
+export const getAllUsersHandler = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await findAllUsers();
+
+    response.status(200).json({
+      status: 'success',
+      result: users.length,
+      data: {
+        users,
+      },
+    });
+  } catch (error: any) {
+    next(error);
+  }
+};
