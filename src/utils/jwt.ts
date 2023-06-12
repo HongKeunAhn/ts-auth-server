5aa551a2cfe91a8cf8dff0c9afc793fb9c5b1007
Keu.ni <skcor8909@gmail.com>
2023-06-12 22:46:44 +0900

    <refactor> refreshToken key 파라미터 추가

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,10 +1,12 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
 import config from 'config';
 
-export const signJwt = (payload: Object, options: SignOptions = {}) => {
-  const privateKey = Buffer.from(config.get<string>('accessTokenPrivateKey'), 'base64').toString(
-    'ascii'
-  );
+export const signJwt = (
+  payload: Object,
+  key: 'accessTokenPrivateKey' | 'refreshTokenPrivateKey',
+  options: SignOptions = {}
+) => {
+  const privateKey = Buffer.from(config.get<string>(key), 'base64').toString('ascii');
 
   return jwt.sign(payload, privateKey, {
     ...(options && options),
@@ -12,14 +14,16 @@ export const signJwt = (payload: Object, options: SignOptions = {}) => {
   });
 };
 
-export const verifyJwt = <T>(token: string): T | null => {
+export const verifyJwt = <T>(
+  token: string,
+  key: 'accessTokenPrivateKey' | 'refreshTokenPrivateKey'
+): T | null => {
   try {
-    const publicKey = Buffer.from(config.get<string>('accessTokenPublicKey'), 'base64').toString(
-      'ascii'
-    );
+    const publicKey = Buffer.from(config.get<string>(key), 'base64').toString('ascii');
 
     return jwt.verify(token, publicKey) as T;
   } catch (error) {
+    console.error(error);
     return null;
   }
 };
